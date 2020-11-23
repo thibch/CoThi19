ede684a7e517e2585dc5ceca9c72fb40e7e7ecba
Thibault Choné <43148335+thibch@users.noreply.github.com>
2020-11-23 11:30:45 +0100

    Test de servlet

---
+++ src/main/java/servlet/ServletTest.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletTest extends HttpServlet {
+
+    public static final String VUE = "/bienvenue.html";
+
+    public ServletTest(){
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.getServletContext().getRequestDispatcher(VUE).forward(req, resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
